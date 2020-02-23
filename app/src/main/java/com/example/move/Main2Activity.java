92384f848b25c6c14acd1fe72124f1cd571357e1
StefanieGlarcher <st.glarcher@gmail.com>
2020-02-23 11:27:05 +0100

    Abbruch Button on second screen

--- app/src/main/java/com/example/move/Main2Activity.java
+++ app/src/main/java/com/example/move/Main2Activity.java
@@ -2,12 +2,15 @@ package com.example.move;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     TextView timer;
+    Button btnAbbrechen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,18 @@ public class Main2Activity extends AppCompatActivity {
 
         timer.setText(minute);
 
+        btnAbbrechen=(Button)findViewById(R.id.btnAbbrechen);
+        btnAbbrechen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
